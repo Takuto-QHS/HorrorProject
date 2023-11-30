5e0802c496cfb2269dba22db5ef3022d6ee5899d
Takuto-QHS <47977631+Takuto-QHS@users.noreply.github.com>
2023-11-30 09:21:06 +0900

    【追加】【変更】点滅させる処理をComponent化
    扉とかの点滅させずにクリックしたいオブジェクトが出てくる可能性を考慮

---
+++ Horror/Assets/Scripts/RaySelect/FlashMaterialComponent.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// このコンポーネント下にある子のMeshRendererを全部拾って
+/// Emissionを使用してflashColorの色で点滅させる
+/// フェード時間やflashColorの強弱はAnimationCurveで設定する
+/// </summary>
+
+public class FlashMaterialComponent : MonoBehaviour
+{
+    [SerializeField]
+    Color flashColor = Color.white;
+    [SerializeField]
+    private AnimationCurve _intensityCurve = AnimationCurve.Linear(0, 0, 1, 1);
+
+    private List<Material> target_material = new List<Material>();
+    private float fadeTime = 0.0f;
+    private float time = 0.0f;
+
+    void Start()
+    {
+        GetMaterials();
+
+        // マテリアルの変更するパラメータを事前に知らせる
+        foreach (Material material in target_material)
+        {
+            material.EnableKeyword("_EMISSION");
+        }
+
+        int length = _intensityCurve.length;
+        if (length < 1)
+            return;
+
+        fadeTime = _intensityCurve.keys[length - 1].time;
+    }
+
+    void Update()
+    {
+        FlashMaterialEmission();
+    }
+
+    void GetMaterials()
+    {
+        // このスクリプトの子のMeshRendererを全部拾う
+        MeshRenderer[] meshRenderers = this.GetComponentsInChildren<MeshRenderer>();
+
+        // 各MeshRendererの中にあるマテリアルをリスト変数に全部入れる
+        foreach (MeshRenderer mesh in meshRenderers)
+        {
+            foreach (Material material in mesh.materials)
+            {
+                target_material.Add(material);
+            }
+        }
+    }
+
+    void FlashMaterialEmission()
+    {
+        time += Time.deltaTime;
+
+        // 内部時刻をfadeTimeで折り返す
+        if (time > fadeTime)
+        {
+            // Mathf.Repeatで0～fadeTimeの範囲の値が得られる
+            time = Mathf.Repeat((float)time, fadeTime);
+        }
+
+        // アニメーションカーブに従ったアルファ値計算
+        float alpha = _intensityCurve.Evaluate((float)time);
+
+
+        // 内部時刻timeにおける明滅状態を反映
+        foreach (Material material in target_material)
+        {
+            material.SetColor("_EmissionColor", new Color(flashColor.r * alpha, flashColor.g * alpha, flashColor.b * alpha));
+        }
+    }
+}
--- Horror/Assets/Scripts/RaySelect/SelectionHighliting.cs
+++ Horror/Assets/Scripts/RaySelect/SelectionHighliting.cs
@@ -5,39 +5,17 @@ using UnityEngine;
 /// <summary>
 /// プレイヤーの視線がオブジェクトに当たった時に
 /// そのオブジェクト全てのアウトラインを表示するスクリプト
-/// ついでにオブジェクトの点滅もさせる
 /// </summary>
 public class SelectionHighliting : IPlayerSelectRayReceive
 {
     private Outline outline;
-
-    [SerializeField]
-    private List<Material> target_material;
-    [SerializeField]
-    private AnimationCurve _intensityCurve = AnimationCurve.Linear(0, 0, 1, 1);
-    [SerializeField]
-    Color flashColor = Color.red;
+    private List<Material> target_material = new List<Material>();
     [SerializeField]
     Color outlineColor = Color.cyan;
-
-    private float fadeTime = 0.0f;
-    private float time = 0.0f;
 
     void Start()
     {
         GetMaterials();
-
-        // マテリアルの変更するパラメータを事前に知らせる
-        foreach (Material material in target_material)
-        {
-            material.EnableKeyword("_EMISSION");
-        }
-
-        int length = _intensityCurve.length;
-        if (length < 1)
-            return;
-
-        fadeTime = _intensityCurve.keys[length - 1].time;
 
         // アウトラインを追加して各種設定
         outline = this.gameObject.AddComponent<Outline>();
@@ -58,33 +36,6 @@ public class SelectionHighliting : IPlayerSelectRayReceive
             {
                 target_material.Add(material);
             }
-        }
-    }
-
-    void Update()
-    {
-        FlashMaterialEmission();
-    }
-
-    void FlashMaterialEmission()
-    {
-        time += Time.deltaTime;
-
-        // 内部時刻をfadeTimeで折り返す
-        if (time > fadeTime)
-        {
-            // Mathf.Repeatで0～fadeTimeの範囲の値が得られる
-            time = Mathf.Repeat((float)time, fadeTime);
-        }
-
-        // アニメーションカーブに従ったアルファ値計算
-        float alpha = _intensityCurve.Evaluate((float)time);
-        
-
-        // 内部時刻timeにおける明滅状態を反映
-        foreach (Material material in target_material)
-        {
-            material.SetColor("_EmissionColor", new Color(flashColor.r * alpha, flashColor.g * alpha, flashColor.b * alpha));
         }
     }
 
